8903f1e5b02795577b0c0eadb3cb6dab6b4d266d
AhmadNajiKam <ahmadnajikam@gmail.com>
2025-02-05 22:29:08 +0300

    Add the basic functionallity of the calculator but didn't test everything

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+let operation = null, accumulator = null, operator = null;
 const Add = (a, b) => {
   return a + b;
 }
@@ -11,29 +12,51 @@ const Mul = (a, b) => {
 }
 
 const Div = (a, b) => {
-  return a / b;
-}
-
-const calcInput = () => {
-  let total = 0, operator1, operator2;
+  if (b != 0)
+    return a / b;
+  else return "error";
 }
 const operate = (op, a, b) => {
   switch (op) {
-    case '+': return Add(a, b);
-    case '-': return Sub(a, b);
-    case '*': return Mul(a, b);
-    case '/': return Div(a, b);
+    case "+": return Add(a, b);
+    case "-": return Sub(a, b);
+    case "*": return Mul(a, b);
+    case "/": return Div(a, b);
   }
 }
+const pressOnEqual = document.querySelector(".equal");
+pressOnEqual.addEventListener('click', () => {
+  const screen = document.querySelector(".screen");
+  accumulator = operate(operation, accumulator, operator);
+  screen.textContent = accumulator;
+});
+
+const calcInput = () => {
+  const ops = document.querySelector(".right");
+  ops.childNodes.forEach(element => {
+    element.addEventListener("click", () => {
+      operation = element.textContent;
+    });
+  })
+}
+
 const populateDisplay = () => {
   const screen = document.querySelector(".screen");
   const buttons = document.querySelector(".left");
   let chosenValue;
   buttons.childNodes.forEach(element => {
     element.addEventListener('click', () => {
-      screen.textContent = Number(element.textContent);
-      chosenValue = Number(element.textContent);
+      screen.textContent = (!isNaN(Number(element.textContent)) ? Number(element.textContent) : element.textContent === '.' ? element.textContent : null);
+      chosenValue = Number(screen.textContent);
+      if (chosenValue === null) {
+        operation = null;
+        accumulator = null;
+        operator = null;
+      }
+      if (operation === null) accumulator = chosenValue;
+      else operator = chosenValue;
     });
   });
 }
 populateDisplay();
+calcInput();
